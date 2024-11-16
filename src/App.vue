31f17d1e7cf15ecc6f88f86fe5e447e18671cc08
DAcosta <daniel.acosta32@outlook.com>
2024-11-16 18:25:12 -0500

    3.5

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-
+    <Header/>
   </div>
 </template>
   
 
 <script>
+import Header from './components/head.vue'
 
+export default {
+    data(){
+        return{
+            name:'App'
+        }
+    }
+};
 </script>
 
 <style Scoped>
---
+++ src/components/head.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="header">
+        <h1 id="title">VULPIN</h1>
+        <h2 class="slogan">Faster Than You Thought</h2>
+
+        <div class="choices">
+            <div class="headItem">Home</div>
+            <div class="headItem">Order</div>
+            <div class="headItem">Profile</div>
+            <div class="headItem">Temp</div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data(){
+        return{
+            name:'Header'
+        }
+    }
+};
+</script>
+
+
+<style Scoped>
+    .header{
+        background-image: linear-gradient(#87ceeb, #07e6fa);
+    }
+    .choices{
+        display: flex;
+        flex-direction: row;
+        flex-wrap: wrap;
+        justify-content: space-around;
+        align-items: baseline;
+    }
+    .headItem{
+        color: #ededed;
+        font-family: "Iceland", sans-serif;
+        font-size: 100;
+        font-weight: 300;
+        font-style: normal;
+    }
+    #title{
+        color: #ededed;
+        font-size: 150;
+        font-weight: 500;
+        font-style: normal;
+    }
+    #slogan{
+        color: #ededed;
+        font-size: 72;
+        font-weight: 300;
+        font-style: normal;
+    }
+</style>
