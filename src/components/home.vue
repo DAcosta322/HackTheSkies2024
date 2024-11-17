73463d984cdf0d6ac828b628de68df3f4fcee712
DAcosta <daniel.acosta32@outlook.com>
2024-11-17 13:07:10 -0500

    v6.5

--- src/components/home.vue
+++ src/components/home.vue
@@ -2,8 +2,19 @@
 Date LM: 16/11/2024
 By: Daniel Acosta*/
 <template>
-    <div id="welcome">WELCOME BACK</div>
-    
+    <div id="welcome">
+        <h1>Welcome Back {{ name }}!!!</h1>
+        <h2>INFO</h2>
+    </div>
+
+    <div id="ad1">
+        <h2>Faster than Ever</h2>
+        <img src="" alt="" srcset="">
+    </div>
+    <div id="ad2">
+        <h2>Seemless Travel</h2>
+        <img src="" alt="" srcset="">
+    </div>
 </template>
 
 <script>
---
+++ src/components/order.vue
@@ -0,0 +1,33 @@
+/*Home Page
+Date LM: 17/11/2024
+By: Daniel Acosta*/
+<template>
+    <form action="" method="post">
+        <label for="restSearch">Search Vulpin</label>
+        <input type="text" name="" id="restSearch" class="restSearch">
+        <br>
+        <br>
+        <div id="filter">
+            <input list="rating">
+            <datalist id="rating">
+                <option value="5">
+                <option value="4.5">
+                <option value="4">
+                <option value="3.5">
+                <option value="3">
+                <option value="2.5">
+                <option value="2">
+            </datalist>
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
